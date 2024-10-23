84b565837bbed0697d95ec90bd0af5562b46b94c
GitBoyAdvance <phosphia2501@gmail.com>
2024-10-23 16:20:29 +0900

    Update set-language.ps1

--- set-language.ps1
+++ set-language.ps1
@@ -1,3 +1,4 @@
+# setup-language.ps1
 # 日本語言語パックをインストール
 Install-WindowsFeature -Name Language-Pack-Ja-JP
 
@@ -7,3 +8,6 @@ Set-WinUserLanguageList -LanguageList ja-JP -Force
 Set-WinSystemLocale ja-JP
 Set-Culture ja-JP
 Set-WinHomeLocation -GeoId 0x7A
+
+# システム再起動（必要に応じて）
+Restart-Computer -Force
